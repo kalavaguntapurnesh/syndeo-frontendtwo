6124b2433fc91bdbff06839ab17109b062bfbfcf
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-09-30 12:59:00 +0530

    Another

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,5 @@
+import AboutOne from "../components/AboutOne";
+import Final from "../components/Final";
 import Footer from "./../components/Footer";
 import Navbar from "./../components/Navbar";
 
@@ -5,7 +7,8 @@ const About = () => {
   return (
     <div>
       <Navbar />
-
+      <AboutOne />
+      <Final />
       <Footer />
     </div>
   );
