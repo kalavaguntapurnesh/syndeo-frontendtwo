13743a2ef736a3e9062bbbd2a3b4decca4a64052
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-12-31 10:35:15 +0530

    Dark Mode Enhance

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -69,7 +69,7 @@ const ResetPassword = () => {
                 <div>
                   <section className="bg-gray-50 my-8">
                     <div className="flex flex-col items-center justify-center px-6 mx-auto">
-                      <div className="w-full bg-white rounded-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                      <div className="w-full bg-white rounded shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-4 sm:p-8">
                           <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-colorThree md:text-2xl dark:text-white">
                             Reset Password
@@ -91,7 +91,7 @@ const ResetPassword = () => {
                                   name="password"
                                   id="password"
                                   value={password}
-                                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                   placeholder="••••••••"
                                   pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                                   title="Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
@@ -113,7 +113,7 @@ const ResetPassword = () => {
 
                             <button
                               type="submit"
-                              className="w-full text-white bg-colorFour hover:bg-colorFour transition ease-in-out duration-1000 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer"
+                              className="w-full text-white bg-colorFour hover:bg-colorFour transition ease-in-out duration-1000 focus:outline-none font-medium rounded text-sm px-5 py-2.5 text-center cursor-pointer"
                             >
                               Update Password
                             </button>
